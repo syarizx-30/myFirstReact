10e800ae8e72bd270dd81b430761bc9debd3af6f
syarizx-30 <rizalsyamsu892@gmail.com>
2025-05-24 20:13:20 +0700

    membuat text di react menggunakan arrow function

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {}
+
+const Card = ({ title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h2>Functional Arrow Component</h2>
+    <Card title="Star Wars" rating={5} isCool={true} actors={[{ name: 'Actors' }]} />
+    <Card title="Avatar" />
+    <Card title="The Lion King" />
+    </div>
+  )
+}
+
+export default App
