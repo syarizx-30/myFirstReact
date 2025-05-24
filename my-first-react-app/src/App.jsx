f2c6b23903543afbb18b3685e5ffc99e9580611d
syarizx-30 <rizalsyamsu892@gmail.com>
2025-05-24 20:45:25 +0700

    menambah css

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 
 const Card = ({ title }) => {
   return (
-    <div>
+    <div className="card" >
       <h2>{title}</h2>
     </div>
   )
@@ -15,9 +15,8 @@ const Card = ({ title }) => {
 
 const App = () => {
   return (
-    <div>
-      <h2>Functional Arrow Component</h2>
-    <Card title="Star Wars" rating={5} isCool={true} actors={[{ name: 'Actors' }]} />
+    <div className="card-container">
+    <Card title="Star Wars" rating={5} isCool={true} />
     <Card title="Avatar" />
     <Card title="The Lion King" />
     </div>
