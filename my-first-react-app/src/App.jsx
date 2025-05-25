9daf0b1afa80c52876aff98a1438be0b30b26558
syarizx-30 <rizalsyamsu892@gmail.com>
2025-05-25 17:11:38 +0700

    States and Hooks

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,19 +1,27 @@
-import React, { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 // function App() {}
 
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card" >
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "♥️" : '🤍'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+  
+
   return (
     <div className="card-container">
     <Card title="Star Wars" rating={5} isCool={true} />
