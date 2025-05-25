1b2637990d2102266957ef1d2a0b9a707cb6438a
syarizx-30 <rizalsyamsu892@gmail.com>
2025-05-25 20:33:47 +0700

    React Snippeets Extensions

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,39 +1,8 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect, useState } from 'react'
-
-// function App() {}
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    <div className="card" onClick= {() => setCount(count + 1)}>
-      <h2>{title} <br /> {count || null}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "♥️" : '🤍'}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-  
-
   return (
-    <div className="card-container">
-    <Card title="Star Wars" rating={5} isCool={true} />
-    <Card title="Avatar" />
-    <Card title="The Lion King" />
-    </div>
+    <div>App</div>
   )
 }
-
 export default App
