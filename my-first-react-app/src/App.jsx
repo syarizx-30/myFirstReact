a36db5dff9bb9d458587c652676f52a1fb8bd33c
syarizx-30 <rizalsyamsu892@gmail.com>
2025-05-26 19:07:57 +0700

    TailWind Css Setup

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+    </div>
   )
 }
 export default App
