920f5c68d649d1611898524155a23477ec57ee30
syarizx-30 <rizalsyamsu892@gmail.com>
2025-05-26 21:02:29 +0700

    Develop header

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import Search from './components/Search'
+import { useState } from 'react'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
-    </div>
+<main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Temukan <span className="text-gradient">Movies</span> yang Dapat Anda Nikmati Tanpa Repot</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>    
+    </main>
   )
 }
 export default App
