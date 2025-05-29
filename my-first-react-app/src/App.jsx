c94a157277150a04f4ef2b908d632c9d1560dbcb
syarizx-30 <rizalsyamsu892@gmail.com>
2025-05-29 21:23:05 +0700

    Tranding Movies Feature

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -52,6 +53,10 @@ const App = () => {
 
 
       setMovieList(data.results  || []);
+      
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
